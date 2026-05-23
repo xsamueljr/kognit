5e1558662d8b496414feb2f9f2858e55af3e318f
xsamueljr <git@xsamueljr.anonaddy.me>
2026-05-23 13:41:28 +0200

    fix: specify site and base needed for github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://xsamueljr.github.io",
+  base: "/kognit",
   vite: {
     plugins: [tailwindcss()]
   }
